0294bee88574f18e45b3b98f3cc05797e5dcda75
fan142 <166977917@qq.com>
2026-04-30 19:47:17 +0800

    feat: 完成v2.0版本，新增input输入输出功能，更新README文档

--- ex01/hello.cpp
+++ ex01/hello.cpp
@@ -1,5 +1,9 @@
 #include<stdio.h>
 int main(){
 	printf("Hello World!\n");
+	char userinput[100];
+	printf("请输入你想要打印的内容：");
+	scanf("%s",userinput);
+	printf("您输入的内容是：%s\n",userinput);
 	return 0;
 } 
